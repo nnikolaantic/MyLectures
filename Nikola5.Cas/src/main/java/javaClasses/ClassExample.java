2421c7de75493b3994c254d6130d2349f315b250
Nikola Antic <77757104+nnikolaantic@users.noreply.github.com>
2022-09-27 21:29:21 +0200

    Update ClassExample.java

--- Nikola5.Cas/src/main/java/javaClasses/ClassExample.java
+++ Nikola5.Cas/src/main/java/javaClasses/ClassExample.java
@@ -12,28 +12,6 @@ import java.util.Scanner;
  */
 public class ClassExample {
     public static void main(String[] args) {
-        String s = "String";
-        String s1 = "StrinG";
-        s.compareTo(s1);
-        
-//        System.out.println(s.compareTo(s1));
-//        System.out.println(s.toLowerCase().compareTo(s1.toLowerCase()));
-        
-//        s = s.toLowerCase();
-//        s1 = s1.toLowerCase();
-
-//        if(s.compareTo(s1) == -1){
-//            System.out.println("Stringovi su razliciti");
-//        }else if (s.compareTo(s1) == 0){
-//            System.out.println("Stringovi su isti");
-//        }else{
-//            System.out.println("Nesto nije u redu sa njima :(");
-//        }
-
-        uporediString(s1, s1);
-        
-//        Scanner scan = new Scanner(System.in);
-//        scan.
         
         Animal a = new Animal();
         System.out.println("I am " + a.whoAmI + " and Im " + a.dogAge + " years old");
@@ -45,14 +23,4 @@ public class ClassExample {
         a.happyDog();
     }
     
-    public static void uporediString(String s1,String s2){
-        String prviString = s1.toLowerCase();
-        String drugiString = s2.toLowerCase();
-
-        if(prviString.compareTo(drugiString) == -1){
-            System.out.println("Stringovi su razliciti");
-        }else{
-            System.out.println("Stringovi su isti");
-        }
-    }
 }
